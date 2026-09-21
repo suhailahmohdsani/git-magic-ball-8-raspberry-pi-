030890ac535fab30d32749e31ed024389718e04e
Suhailah Mohd Sani <suhailahmohdsani@gmail.com>
2026-09-21 12:18:11 +0800

    new version

--- compass.py
+++ compass.py
@@ -1,31 +1,53 @@
 from sense_hat import SenseHat
+import time
 
 sense = SenseHat()
 
 sense.set_imu_config(
     compass_enabled=True,
-    gyro_enabled=True,
+    gyro_enabled=False,
     accel_enabled=False
 )
 
-while True:
-    heading = sense.get_compass()
+# Simple 3x5 font
+font = {
+    "0": ["111", "101", "101", "101", "111"],
+    "1": ["010", "110", "010", "010", "111"],
+    "2": ["111", "001", "111", "100", "111"],
+    "3": ["111", "001", "111", "001", "111"],
+    "4": ["101", "101", "111", "001", "001"],
+    "5": ["111", "100", "111", "001", "111"],
+    "6": ["111", "100", "111", "101", "111"],
+    "7": ["111", "001", "001", "001", "001"],
+    "8": ["111", "101", "111", "101", "111"],
+    "9": ["111", "101", "111", "001", "111"],
+}
 
-    if heading >= 337.5 or heading < 22.5:
-        direction = 'N'
-    elif heading < 67.5:
-        direction = 'NE'
-    elif heading < 112.5:
-        direction = 'E'
-    elif heading < 157.5:
-        direction = 'SE'
-    elif heading < 202.5:
-        direction = 'S'
-    elif heading < 247.5:
-        direction = 'SW'
-    elif heading < 292.5:
-        direction = 'W'
+def get_direction(degree):
+    if degree >= 337.5 or degree < 22.5:
+        return "N"
+    elif degree < 67.5:
+        return "NE"
+    elif degree < 112.5:
+        return "E"
+    elif degree < 157.5:
+        return "SE"
+    elif degree < 202.5:
+        return "S"
+    elif degree < 247.5:
+        return "SW"
+    elif degree < 292.5:
+        return "W"
     else:
-        direction = 'NW'
+        return "NW"
 
-    sense.show_message(f"{direction} {heading:.1f}")
+while True:
+    degree = sense.get_compass()
+    direction = get_direction(degree)
+
+    sense.clear()
+
+    # Show direction as a large letter
+    sense.show_letter(direction[0])
+
+    time.sleep(0.5)
