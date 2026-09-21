c05599dec68d0c03409ac57f1db62d7470a04c9b
Suhailah Mohd Sani <suhailahmohdsani@gmail.com>
2026-09-21 14:21:14 +0800

    updated version

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from sense_hat import SenseHat
 
 sense = SenseHat()
 
-sense.show_message('Ask Me A Question Bootiful')
+sense.show_message('Ask Me A Question Bootiful', text_colour=(255, 0, 255), scroll_speed=0.03)
 sleep(3)
 
-replies = ['i think so bro', 'i dunno abt that...', 'YES! CONFORM', 'HELL NAH!!!!', 'yu yao disagree lmao']
+replies = [('i dunno abt that...', (255, 255, 0)), ('YES! CONFORM', (0, 255, 0)), ('yu yao disagree lmao', (255, 0, 0))]
 
 while True:
     x, y, z = sense.get_accelerometer_raw().values()
@@ -16,10 +16,8 @@ while True:
     y = abs(y)
     z = abs(z)
 
-    if x > 2 or y > 2 or z > 2 :
-        sense.show_message(choice(replies))
+    if x > 2 or y > 2 or z > 2:
+        reply, colour = choice(replies)
+        sense.show_message(reply, text_colour=colour, scroll_speed=0.03)
     else:
         sense.clear()
-
-
-
