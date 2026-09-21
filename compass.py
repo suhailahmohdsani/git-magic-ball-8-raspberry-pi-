fc797e666b7e9ae6de78f19a6953c4bf4e55ba3b
Suhailah Mohd Sani <suhailahmohdsani@gmail.com>
2026-09-21 12:08:29 +0800

    compass

---
+++ compass.py
@@ -0,0 +1,25 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+while True:
+    heading = sense.get_compass()
+
+    if heading >= 337.5 or heading < 22.5:
+        direction = 'N'
+    elif heading < 67.5:
+        direction = 'NE'
+    elif heading < 112.5:
+        direction = 'E'
+    elif heading < 157.5:
+        direction = 'SE'
+    elif heading < 202.5:
+        direction = 'S'
+    elif heading < 247.5:
+        direction = 'SW'
+    elif heading < 292.5:
+        direction = 'W'
+    else:
+        direction = 'NW'
+
+    sense.show_message(f"{direction} {heading:.1f}")
